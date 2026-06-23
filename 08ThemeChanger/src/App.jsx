c1a61fdf9b8899c4bde50808b75d0ecf3f948aef
Rishabh <rm1994269@gmail.com>
2026-06-23 23:58:02 +0530

    Implement theme context and components; add Card and ThemeBtn for theme toggling
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 08ThemeChanger/src/App.jsx
+++ 08ThemeChanger/src/App.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { ThemeProvider } from './Context/Theme'
+import Card from './Compnents/Card'
+import ThemeBtn from './Compnents/Themebtn'
+import { useEffect } from 'react'
 
 const App = () => {
+  const [ThemeMode, setThemeMode] = useState('light');
+  const darkTheme = () => setThemeMode('dark');
+  const lightTheme = () => setThemeMode('light');
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark');
+    document.querySelector('html').classList.add(ThemeMode);
+  }, [ThemeMode]);
+
   return (
-    <div>App</div>
+    <ThemeProvider value={{ThemeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <ThemeBtn />
+            </div>
+                <Card />
+            <div className="w-full max-w-sm mx-auto">
+                
+            </div>
+        </div>
+      </div>
+ </ThemeProvider>
   )
 }
 

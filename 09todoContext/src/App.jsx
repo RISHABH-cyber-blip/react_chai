9e9ba7cf4447e4792cd7336ad4afecdbda384bbb
Rishabh <rm1994269@gmail.com>
2026-06-27 22:41:35 +0530

    basic context functionality complete
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 09todoContext/src/App.jsx
+++ 09todoContext/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { ToDoProvider } from './contexts'
 
 const App = () => {
+  const [todos, setTodos] = useState([])
+  
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [...prevTodos, {id: Date.now(), ...todo}])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev)=>prev.filter((prevTodo)=>(prevTodo.id !== id)))
+  }
+  
+  const toggleTodoCompletion = (id) => {
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)))
+  }
+
   return (
-   <>
+   <ToDoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleTodoCompletion}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -14,7 +33,7 @@ const App = () => {
           </div>
       </div>
      </div>
-   </>
+   </ToDoProvider>
   )
 }
 

9432bdf20c99429e668649882ddd8878c9fbc4ba
Rishabh <rm1994269@gmail.com>
2026-06-28 22:25:07 +0530

    feat: implement TodoItem component with edit and delete functionality
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 09todoContext/src/App.jsx
+++ 09todoContext/src/App.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import { ToDoProvider } from './contexts'
+import TodoItem from './components/ToDoItem'
 
 const App = () => {
   const [todos, setTodos] = useState([])
@@ -35,10 +36,15 @@ const App = () => {
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-              {/* Todo form goes here */} 
+              <ToDoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo)=>(
+                <div key={todo.id} className='w-full'>
+                   <TodoItem todo={todo} />
+                </div>
+              ))}
           </div>
       </div>
      </div>

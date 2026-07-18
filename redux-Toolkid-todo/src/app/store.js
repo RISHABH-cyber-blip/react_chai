12b840b19af7ae479e7d80318a58a3c7bf474cf1
Rishabh <rm1994269@gmail.com>
2026-07-18 21:46:26 +0530

    refactor: remove explicit Redux DevTools configuration for production
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- redux-Toolkid-todo/src/app/store.js
+++ redux-Toolkid-todo/src/app/store.js
@@ -5,11 +5,6 @@ export const store = configureStore({
     reducer: {
         todos: todoReducer
     }
-    ,
-    devTools: process.env.NODE_ENV !== 'production' ? { name: 'redux-Toolkid-todo' } : false
 })
 
-// Enable Redux DevTools explicitly in development for clearer inspection
-// configureStore enables DevTools by default, but an explicit option
-// and a store name can help some browser setups and extensions.
 export default store;

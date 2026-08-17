366afc8ff08ce629f6d81689dee2f1701486e3da
Rishabh <rm1994269@gmail.com>
2026-08-17 20:12:56 +0530

    lot of commit
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 03bgChanger/src/App.jsx
+++ 03bgChanger/src/App.jsx
@@ -3,13 +3,30 @@ import './index.css'
 
 const App = () => {
   const [bgColor, setBgColor] = React.useState('white')
+  const reset = (color) => {
+    if (color === 'white') {
+      return "white text-white bg-black border border-gray-300 p-2 rounded w-20 h-10 m-4" 
+    }
+    else {
+      return "bg-white text-black border border-gray-300 p-2 rounded w-20 h-10 m-4"
+    }
+  }
+
+  const handleResetButton = (color) => {
+    if(color!=='white') {
+      setBgColor('white')
+    }
+        
+  }
+
 
   return (
     <div className={bgColor} style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <div className="bg-black">
+      <div className="bg-black" width="100%" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
         <button className="blue" onClick={() => setBgColor('blue')}>blue</button>
         <button className="green" onClick={() => setBgColor('green')}>green</button>
         <button className="red" onClick={() => setBgColor('red')}>red</button>
+        <button className={reset(bgColor)} onClick={() => handleResetButton(bgColor)}>reset</button>
       </div>
     </div>
   )

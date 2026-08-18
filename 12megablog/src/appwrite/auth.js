88fe1bed56d54b8e7dfa6c9f5d89beb0bbe5a955
Rishabh <rm1994269@gmail.com>
2026-08-18 20:32:37 +0530

    login and sign in
    Signed-off-by: Rishabh <rm1994269@gmail.com>

---
+++ 12megablog/src/appwrite/auth.js
@@ -0,0 +1,46 @@
+import { Client, Account, ID } from "appwrite";
+import { conf } from "../conf/conf.js";
+
+export class AuthService {
+    Client=new Client();
+    account;
+
+    constructor(){
+        this.Client
+         .setEndpoint(conf.appWriteUrl)
+         .setProject(conf.appWriteProjectId);
+        
+         this.account=new Account(this.Client);
+
+    }
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount=await this.account.create(ID.unique(),email,password,name);
+            if (userAccount) {
+                return this.login({email,password});
+            } else {
+                return userAccount;
+            }
+            
+        } catch (error) {
+            throw error
+        }
+
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailSession(email,password);
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+
+
+
+const authService=new AuthService();
+
+export default authService;

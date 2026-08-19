ce6df1e72d85c43ec0b14aa2e9a3c49338c4d71c
Rishabh <rm1994269@gmail.com>
2026-08-19 21:11:51 +0530

    createpost function
    Signed-off-by: Rishabh <rm1994269@gmail.com>

---
+++ 12megablog/src/appwrite/config.js
@@ -0,0 +1,41 @@
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+import { conf } from "../conf/conf.js";
+
+export class Service{
+    Client=new Client();
+    Databases
+    bucket
+
+     constructor(){
+            this.Client
+             .setEndpoint(conf.appWriteUrl)
+             .setProject(conf.appWriteProjectId);
+            
+             this.databases=new Databases(this.Client);
+             this.bucket=new Storage(this.Client);
+    
+        }
+    async createPost({title,slug,content,featuredImage,userId,status}){
+         try {
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    userId,
+                    status
+                }
+            )
+         } catch (error) {
+            console.log("error in createPost:", error);
+         }
+    }
+        
+}
+
+const service=new Service();
+
+export default service;

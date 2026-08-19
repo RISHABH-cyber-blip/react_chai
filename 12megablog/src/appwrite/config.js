ff7364d041132aee9f6e61d89911f8de818eb864
Rishabh <rm1994269@gmail.com>
2026-08-19 22:13:58 +0530

    get post and posts app write
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 12megablog/src/appwrite/config.js
+++ 12megablog/src/appwrite/config.js
@@ -59,9 +59,55 @@ export class Service{
                 conf.appWriteCollectionId,
                 slug
             )
+            return true;
         } catch (error) {
             console.log("error in deletePost:", error);
+            return false;
         }
+    }
+    
+    async getPost(slug){
+       try {
+          return this.Databases.getDocument(
+            conf.appWriteDatabaseId,
+            conf.appWriteCollectionId,
+            slug
+          )
+       } catch (error) {
+        console.log("error in getPost",error)
+        return false;
+        
+       }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+       try {
+         return await this.databases.listDocuments(
+            conf.appWriteDatabaseId,
+            conf.appWriteCollectionId,
+            queries
+         )
+       } catch (error) {
+         console.log("error in getposts",error)
+         return false;
+       }
+    }
+
+    //file upload service
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            
+        }
+    }
+
+    async deleteFile(){
+        
     }
         
 }

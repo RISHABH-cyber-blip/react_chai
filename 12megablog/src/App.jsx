71eb32700e5668475ef062cb69504c2ec5ae0643
Rishabh <rm1994269@gmail.com>
2026-08-24 21:16:43 +0530

    laoding and logout feature
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import { useDispatch } from "@reduxjs/toolkit"
+import "index.css"
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div>
-      App hello miss world</div>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser(dispatch)
+    .then((userData)=>{
+        if(userData) {
+           dispatch(login(userData))
+        }
+        else{
+           dispatch(logout())
+        }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
+
+  return !loading ? (
+    <div className="flex justify-center items-center h-screen text-2xl font-bold">
+       Loading....
+       <Header />
+       <main>
+         {/*<Outlet />*/}
+       </main>
+       <Footer />
+    </div>
+  ): null
+  
 }
 
 export default App

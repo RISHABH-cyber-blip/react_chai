d7e6c5e236f8114b0eadbe9095b20d91eacbd069
Rishabh <rm1994269@gmail.com>
2026-08-24 20:41:59 +0530

    auth logout and header and footer initilize
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 12megablog/src/store/authSlice.js
+++ 12megablog/src/store/authSlice.js
@@ -12,6 +12,14 @@ const authSlice = createSlice({
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
         }
     }
 })
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;

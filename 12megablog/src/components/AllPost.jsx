45b5bedd191e4f170654296ef464fdaf8a84d2b9
Rishabh <rm1994269@gmail.com>
2026-09-16 22:59:56 +0530

    allpost
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 12megablog/src/components/AllPost.jsx
+++ 12megablog/src/components/AllPost.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import service from '../appwrite/config'
-import {Conbtainer,PostCard} from '../components/index'
+import {Container,PostCard} from '../components/index'
 
 const AllPost = () => { 
+    const [posts,setPosts] = React.useState([])
+
+    React.useEffect(() => {},[])
+    service.getPosts([]).then((posts) => {
+        if(posts){
+            setPosts(posts)
+        }
+    })
+
   return (
-    <div>AllPost</div>
+    <div className='w-full h-full flex justify-center items-center'>
+      <Container>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            {posts.map((post) => (
+                <div className='col-span-1' key={post.$id}>
+                    <PostCard key={post.$id} {...post} />
+                </div>
+            ))}
+        </div>
+      </Container>
+    </div>
   )
 }
 

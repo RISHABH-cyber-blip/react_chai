3754a3f316b30fbb46fd43c292eb2e03a5b8c392
Rishabh <rm1994269@gmail.com>
2026-09-16 23:23:41 +0530

    Home page
    Signed-off-by: Rishabh <rm1994269@gmail.com>

--- 12megablog/src/pages/Home.jsx
+++ 12megablog/src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {Container,AllPost} from '../components/index'
+import service from '../appwrite/config'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [posts,setPosts] = useState([])
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if(posts){
+                setPosts(posts.documents)
+            }
+        })
+    },[])
+  
+    if(posts.length === 0){
+        return (
+            <div className='w-full h-full flex justify-center items-center'>
+                <Container>
+                    <h1 className='text-2xl font-bold'>No posts found</h1>
+                </Container>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div className='w-full h-full flex justify-center items-center'>
+                <Container>
+                    <AllPost posts={posts} />
+                </Container>
+            </div>
+        )
+    }
+
 }
 
 export default Home
